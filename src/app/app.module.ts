1d626dbd103f32c60664a876d139e5bca38f61a0
SilverFlame83 <imren_83@abv.bg>
2021-08-26 22:55:37 +0300

    footer implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { HomeComponent } from './core/home/home.component';
 import { CoursesComponent } from './core/courses/courses.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    CoursesComponent
+    CoursesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
